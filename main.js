52b608212a036d00c025e0dbc23eebbda9c091fb
ahsan-tikito <ahsan@tikito.app>
2021-12-30 22:57:53 +0500

    styles file

---
+++ main.js
@@ -0,0 +1,88 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDp7FUCnZU3vsMiKXluqjaNRFZN5_HbB1A",
+    authDomain: "iot-project-183.firebaseapp.com",
+    databaseURL: "https://iot-project-183-default-rtdb.firebaseio.com",
+    projectId: "iot-project-183",
+    storageBucket: "iot-project-183.appspot.com",
+    messagingSenderId: "19596243819",
+    appId: "1:19596243819:web:47d20ab0f5e05d359dd22e"
+  };
+const app = firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+database.ref('Humidity').on('value',(snapshot)=>{
+    fetchedData = Object.values(snapshot.val());
+    console.log(fetchedData);
+    lineChart(fetchedData, 'humidityChart', "Humidity");
+    barChart(fetchedData, 'humidityBarChart', "Humidity");
+})
+database.ref('Temperature').on('value',(snapshot)=>{
+    fetchedData = Object.values(snapshot.val());
+    console.log(fetchedData);
+    lineChart(fetchedData, 'temperatureChart', 'Temperature');
+    barChart(fetchedData, 'temperatureBarChart', 'Temperature');
+})
+function lineChart(fetchedData, id,label) {
+        const ctx = document.getElementById(id).getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100],
+                datasets: [{
+                    label: label,
+                    data: fetchedData,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            }
+        });
+}
+
+function barChart(fetchedData, id,label) {
+    const ctx = document.getElementById(id).getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100],
+            datasets: [{
+                label: label,
+                data: fetchedData,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+    });
+}
+
+
+
+
